57d9fa18f06ec70e7b8f0f92986ce24649105c5f
Blacksypha <sypha.0@gmail.com>
2026-04-16 14:24:20 -0400

    Add new mcp tool to edit document

--- mcp_server.py
+++ mcp_server.py
@@ -13,19 +13,31 @@ docs = {
     "spec.txt": "These specifications define the technical requirements for the equipment.",
 }
 
-# TODO: Write a tool to read a doc
 @mcp.tool(
     name="read_doc_contents",
     description="Read the contents of a document and return it as a string."
 )
-def read_documents(
+def read_document(
     doc_id: str = Field(description="The ID of the document to read.")
 ):
     if doc_id not in docs:
         raise ValueError(f"Document with ID '{doc_id}' not found.")
     return docs[doc_id]
 
-# TODO: Write a tool to edit a doc
+@mcp.tool(
+    name="edit_doc_contents",
+    description="Edit a document by replacing a string in the documents with a new string."
+)
+def edit_document(
+    doc_id: str = Field(description="The ID of the document to edit."),
+    old_str: str = Field(description="The text to be replaced. Must match exactly, including whitespace."),
+    new_str: str = Field(description="The new text to insert in place of the old text.")
+):
+    if doc_id not in docs:
+        raise ValueError(f"Document with ID '{doc_id}' not found.")
+    docs[doc_id] = docs[doc_id].replace(old_str, new_str)
+    return docs[doc_id]
+
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
